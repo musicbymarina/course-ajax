e27feb6962ff3e497b5ac76973a3dc641a7feb15
Marina <musicbymarina@gmail.com>
2018-03-24 03:59:15 +0100

    Update app.js
    Add some classes for css display

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,99 +1,98 @@
 (function () {
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
-    let searchedForText = 'computer';
+    let searchedForText;
     const responseContainer = document.querySelector('#response-container');
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
-    
-	/**@description Create a request to Pixabay API (nicer pictures than in unsplash) : 
-	* create a new request and open a Get request (don't need to set the request header)
-	* if I have a response I add an image
-	* if I have nothing, I will prevent the user I have no images for him
-	*/
-	const pixabayId = '8326628-dc9ac48259cd959e1aeaf66d8';
-	const pixabayUrl = `https://pixabay.com/api/?key=8326628-dc9ac48259cd959e1aeaf66d8&q=${searchedForText}&image_type=photo&pretty=true`;
-	
-	const pixabayRequest = new XMLHttpRequest();
-	pixabayRequest.open('GET', pixabayUrl);
-	pixabayRequest.onload = addImage;
-	pixabayRequest.onerror = function(err) {
-		sendError(err, 'images');
-	};
-	pixabayRequest.send();
+        
+       
+       	/** @description - Insert an image in the response Container 
+       	*	If there's no image, insert a message for the user
+       	* @param data - Javascript object converted, from the server
+       	*/
+    	function addImage(data) {
+     	let htmlContent;
+     	const firstImage = data.hits[0];
+     	
+     		if(firstImage) {
+     			htmlContent = `<figure>
+     			<img src="${firstImage.webformatURL}" alt="${firstImage.tags}">
+     			<figcaption>${firstImage.tags}</figcaption>
+     			</figure>`;
+     			responseContainer.insertAdjacentHTML("afterbegin", htmlContent);
+     		} 
+     		else {
+     		htmlContent = `<h2>Sorry guys...</h2>
+     		<p class="error-no-image">We didn't find your ${part} about ${searchedForText}</p>`;
+     		responseContainer.insertAdjacentHTML("afterbegin", htmlContent);
+     		}
+     	}   
+     
+        /** @description - Insert some articles in the response Container, after the image
+       	*	If there's no articles, insert a message for the user
+       	* 	@param data - Javascript object converted, from the server
+       	*/
+     	function addArticles(data) {
+     	let htmlContent;
+     	const articles = data.response.docs;
+     	
+     		if(articles.length > 1) {
+     			articles.map(article => {
+     				htmlContent = `<li class="article">
+     				<h2> <a href="${article.web_url}">${article.headline.main} </a> </h2>
+     				<p> ${article.snippet} </p>
+     				</li>`;
+     				responseContainer.insertAdjacentHTML("beforeend", `<ul>${htmlContent}</ul>`);
+     			});
+     		}
+     		else {
+     			htmlContent = `<h2>Sorry guys...</h2>
+     			<p class="error-no-image">We didn't find your ${part} about ${searchedForText}</p>`;
+     			responseContainer.insertAdjacentHTML("beforeend", htmlContent);
+     		}
+     	}
+        
+        /** @description - Log your error if there's no content and display the error on the page
+       	*	@param err - Description of the error
+       	* 	@param part - Part of the content (image / articles)
+       	*/
+        function sendError(err, part) {
+        	console.log(err);
+        	let htmlContent = `<h2>Sorry guys...</h2>
+     		<p class="error-no-articles">We didn't find your ${part} about ${searchedForText}</p>`;
+        }
+        
+        
+        /** @description - Search the request through the network to Pixabay API, 
+        *	Return the converted json response, display it
+       	*	If there's no image, insert a message for the user
+       	*/
+       	// Pixabay URL for fetch
+        const pixabayUrl = `https://pixabay.com/api/?key=8326628-dc9ac48259cd959e1aeaf66d8&q=${searchedForText}&image_type=photo&pretty=true`;
 
-	function addImage() {
-	let htmlContent;
-	const data = JSON.parse(this.responseText);
-	// if I can translate JSON response from Pixabay to a Javascript object, and if I can get the first image
-	if(data.hits && data.hits[0]) {
-		const firstImage = data.hits[0];
-		htmlContent = `<figure>
-		<img src='${firstImage.webformatURL}' alt='${firstImage.description}'>
-		<figcaption>${firstImage.tags}</figcaption>
-		</figure>`;
-		responseContainer.insertAdjacentHTML("afterbegin", htmlContent);
-	} else {
-		const noImages = `<h3>Sorry guys...</h3>
-		<p>No images about ${searchedForText}, try again...</p>`;
-		responseContainer.insertAdjacentHTML("afterbegin", noImages);
-	}
-};
-
-	/**@description Send an error message and tell which part I need to change in the text (articles or images) 
-	* @param error - Error message 
-	* @param part - Part of the text I need to change (articles OR images)
-	*/
-	function sendError(error, part) {
-	const text = `<h3>Sorry guys...</h3>
-		<p>No ${part} about ${searchedForText}, try again...</p>`;
-	console.log(error);
-	responseContainer.insertAdjacentHTML("afterbegin", text);
-	};
-
-	
-	/**@description Create a request to NY Times API : 
-	* create a new request and open a Get request
-	* fill header informations for API server (my ID)
-	* if I have a response I add all the articles
-	* if I have nothing, I will prevent the user I have no articles for him
-	*/
-	
-	const NYTimesId = '10466aca3c664718a30b7c6023fbd2bb';
-	const NYTimesUrl = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=${NYTimesId}`;
-	
-	const NYRequest = new XMLHttpRequest();
-	NYRequest.open('GET', NYTimesUrl);
-	NYRequest.onload = addArticles;
-	NYRequest.onerror = function(err) {
-		sendError(err, 'articles');
-	};
-	NYRequest.send();
-	
-	function addArticles() {
-		const articles = JSON.parse(this.responseText);
+        fetch(pixabayUrl)
+        .then(response => response.json())
+        .then(addImage)
+        .catch(err => sendError(err, 'images')
+        );
+        
+        /** @description - Search the request through the network to NY Times API, 
+        *	Return the converted json response, display it
+       	*	If there's no articles, insert a message for the user
+       	*/
+       	// NY Times ID and URL for fetch
+        const nyId = '10466aca3c664718a30b7c6023fbd2bb';
+		const nyUrl = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=${nyId}`;
 		
-		if(articles.response && articles.response.docs && articles.response.docs.length > 1) {
-			articles.response.docs.map(function(article) {
-				let listeArticles = `<li>
-		<h4><a href="${article.web_url}">${article.headline.main}</a></h4>
-		<p>${article.snippet}</p>
-		</li>`;
-		
-		responseContainer.insertAdjacentHTML('beforeend', `<ul>${listeArticles}</ul>`);
-			});	
-		} else {
-			const noArticles = `<h3>Sorry guys...</h3>
-	<p>No articles about ${searchedForText}, try again...`;
-	responseContainer.insertAdjacentHTML('beforeend', noArticles);
-		}
-	}    
+		fetch(nyUrl)
+		.then(response => response.json())
+		.then(addArticles)
+		.catch(err => sendError(err, 'articles')
+		);
+
     });
-    
 })();
-
-
-
